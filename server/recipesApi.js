8f8c2c6d0ee406443baad64eb4cf38897378f5d8
Neutix <martin.wedvik@gmail.com>
2022-03-14 18:48:15 +0100

    Initial commit

---
+++ server/recipesApi.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+export function RecipesApi(mongoDatabase) {
+    const router = new Router();
+
+    router.get("/", async (req, res) => {
+        const recipe = await mongoDatabase
+            .collection("Food-recipes")
+            .find(
+            )
+            .map(({ _id, name , type, recipeType, time, difficulty, topText, ingredients, img }) => ({
+                _id,
+                name,
+                type,
+                recipeType,
+                time,
+                difficulty,
+                topText,
+                ingredients,
+                img,
+            }))
+            .limit(10)
+            .toArray();
+        res.json(recipe);
+    });
+
+    router.post("/new", (req, res) => {
+        const { title } = req.body;
+        const result = mongoDatabase.collection("recipe").insertOne({
+            title,
+        });
+        res.sendStatus(500);
+    });
+
+    return router;
+}
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import * as path from "path";
+import { RecipesApi } from "./recipesApi.js";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
+mongoClient.connect().then(async () => {
+    console.log("Connected to mongodb");
+    app.use(
+        "/api/recipes",
+        RecipesApi(mongoClient.db(process.env.MONGODB_DATABASE || "pg6301-7"))
+    );
+});
+
+app.use(express.static("../client/dist/"));
+
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Started on http://localhost:${server.address().port}`);
+});
+
+
+
